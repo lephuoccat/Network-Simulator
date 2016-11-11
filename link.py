478a1be1c5943585c272d18f9d1bc6ff121a0a1c
catle <calvine.le@gmail.com>
2016-11-10 19:09:31 -0800

    Added send the packet in the buffer

--- link.py
+++ link.py
@@ -19,6 +19,15 @@ class Buffer:
         else:
             self.logger.log_link_dropped_packet_buffer_full(self.link.identifier, packet)
 
+	#Retrieves the next packet from the buffer in order
+    def get(self):
+        (packet, destination) = self.queue.get_nowait()
+        self.available_space += packet.size
+        self.logger.log_link_buffer_available_space(self.link.identifier, self.available_space)
+        return (packet, destination)
+
+
+
 class Link:
 
 	def __init__(self, identifier, rate, delay, buffer_size, deviceA, deviceB):
@@ -28,4 +37,37 @@ class Link:
         self.buffer = Buffer(buffer_size, self)
         self.deviceA = deviceA
         self.deviceB = deviceB
+        self.busy = False
         
+    # Sends a packet instantly if the link is not busy
+    # or enqueues the packet in the buffer if the link is busy
+    def send_packet(self, packet, sender):
+
+        # The recipient is whatever device is not the sender
+        recipient = self.other_device(sender)
+        # Place in buffer if busy, otherwise send now
+        if not self.busy:
+            self._send_packet_now(packet, recipient)
+            self.logger.log_link_sent_packet_immediately(self.identifier, packet)
+        else:
+            self.buffer.put(packet, recipient)
+
+    # Called internally to send a packet by scheduling the relevant events
+    def _send_packet_now(self, packet, recipient):
+        assert not self.busy
+        self.busy = True
+
+        sending_delay = packet.size / self.rate
+        self.event_scheduler.delay_event(sending_delay + self.delay, PacketArrivalEvent(packet, recipient, self))
+        self.event_scheduler.delay_event(sending_delay, LinkReadyEvent(self))
+
+    # Send a packet from the buffer, if possible
+    def wake(self):
+        self.busy = False
+        try:
+            # If there are any packets in the buffer, send one
+            (packet, destination) = self.buffer.get()
+            self._send_packet_now(packet, destination)
+            self.logger.log_link_sent_packet_from_buffer(self.identifier, packet)
+        except Queue.Empty:
+            pass
